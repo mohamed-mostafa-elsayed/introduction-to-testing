f738bc1377cbcc1dfca4e24dac38e0feac114d70
Mohammed mostafa <mohamed.mostafa.elsayed@oceanbluesoftware.co.uk>
2025-11-09 01:54:33 +0200

    Testing the DOM

--- examples/accident-counter/src/counter.test.jsx
+++ examples/accident-counter/src/counter.test.jsx
@@ -1,44 +1,130 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Counter } from './counter';
 
 import '@testing-library/jest-dom/vitest';
 
-describe.todo('Counter ', () => {
-  beforeEach(() => {
+describe('Counter ', () => {
+  it('renders with an initial count of 0', () => {
     render(<Counter />);
+
+    const counter = screen.getByTestId("counter-count");
+
+    expect(counter).toHaveTextContent("0");
+    expect(counter.textContent).toBe("0");
+
   });
 
-  it('renders with an initial count of 0');
+  it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
+    render(<Counter />);
 
-  it('disables the "Decrement" and "Reset" buttons when the count is 0');
 
-  it.todo('displays "days" when the count is 0', () => {});
+    const decrementBtn  = screen.getByRole('button', { name: /decrement/i });
+    const resetBtn  = screen.getByRole('button', { name: /reset/i });
 
-  it.todo(
-    'increments the count when the "Increment" button is clicked',
-    async () => {},
+    expect(decrementBtn).toBeDisabled();
+    expect(resetBtn).toBeDisabled();
+
+  });
+
+  it('displays "days" when the count is 0', () => {
+    render(<Counter />);
+
+    const days = screen.getByTestId("counter-unit");
+
+    expect(days).toHaveTextContent("days");
+  });
+
+  it(
+    'increments the count when the "Increment" button is clicked', async () => {
+      render(<Counter />);
+
+      await act(async () => {
+        await userEvent.click(screen.getByRole('button', { name: /increment/i }));
+      });
+
+      expect(screen.getByTestId("counter-count")).toHaveTextContent("1");
+    },
   );
 
-  it.todo('displays "day" when the count is 1', async () => {});
+  it('displays "day" when the count is 1', async () => {
+    render(<Counter />);
 
-  it.todo(
-    'decrements the count when the "Decrement" button is clicked',
-    async () => {},
+    const days = screen.getByTestId("counter-unit");
+
+    await userEvent.click(screen.getByRole('button', { name: /increment/i }));
+
+
+    expect(days).toHaveTextContent("day");
+
+  });
+
+  it(
+    'decrements the count when the "Decrement" button is clicked', async () => {
+      render(<Counter initialCount={1} />);
+
+      const counter = screen.getByTestId("counter-count");
+      const decrementBtn = screen.getByRole('button', { name: /decrement/i });
+
+      expect(decrementBtn).not.toBeDisabled();
+
+      await act(async () => {
+        await userEvent.click(decrementBtn);
+      });
+
+      expect(decrementBtn).toBeDisabled();
+
+
+      expect(counter).toHaveTextContent("0");
+
+
+
+    },
   );
 
-  it.todo('does not allow decrementing below 0', async () => {});
+  it('does not allow decrementing below 0', async () => {
+    render(<Counter initialCount={0} />);
 
-  it.todo(
+    const counter = screen.getByTestId("counter-count");
+    const decrementBtn = screen.getByRole('button', { name: /decrement/i });
+
+
+    expect(decrementBtn).toBeDisabled();
+
+
+    await act(async () => {
+      await userEvent.click(decrementBtn);
+    });
+
+    expect(counter).toHaveTextContent("0");
+
+  });
+
+  it(
     'resets the count when the "Reset" button is clicked',
-    async () => {},
+    async () => {
+      render(<Counter initialCount={10} />);
+
+      const counter = screen.getByTestId("counter-count");
+
+      await act(async () => {
+        await userEvent.click(screen.getByRole('button', { name: /reset/i }));
+      });
+
+      expect(counter).toHaveTextContent("0");
+
+    },
   );
 
-  it.todo(
-    'disables the "Decrement" and "Reset" buttons when the count is 0',
-    () => {},
-  );
+  it('updates the document title based on the count', async () => {
+    render(<Counter />);
 
-  it.todo('updates the document title based on the count', async () => {});
+    await act(async () => {
+      await userEvent.click(screen.getByRole('button', { name: /increment/i }));
+    });
+
+    expect(document.title).toEqual(expect.stringContaining("1"));
+
+  });
 });
