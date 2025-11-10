53cacd32d8eab452252db43f6d44bc3de5aca916
Mohammed mostafa <mohamed.mostafa.elsayed@oceanbluesoftware.co.uk>
2025-11-10 03:17:07 +0200

    Stubs, Spies, and Mocks

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,5 +1,21 @@
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
+
+const spyLog = vi.spyOn(console, 'log');
+
+const spyRandom = vi.spyOn(Math, "random").mockImplementation(() => 0.5);
 
 test('a super simple test', () => {
-  expect(true).toBe(true);
+  console.log("hello world");
+
+  expect(spyLog).toHaveBeenCalled();
+  expect(spyLog).toHaveBeenCalledWith('hello world');
+  expect(spyLog).toBeCalledTimes(1);
+  expect(spyLog).toHaveBeenCalledOnce();
+});
+
+
+test('simple mock', () => {
+  const random = Math.random();
+
+  expect(random).toBe(0.5);
 });
